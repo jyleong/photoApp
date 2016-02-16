165196a637c3ae0cb98e522a0969bfde784f7187
jamesleong <jyleong@sfu.ca>
2016-02-16 02:34:59 +0000

    sizing for images and changed index html of images page

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,4 +3,13 @@ class Image < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
   ## carrerir wave to associate image with the model we have
   ##mount_uploader method we use
+  validate :picture_size
+  
+  private
+  
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:pictures, "should be less than 5MB")
+    end
+  end
 end
